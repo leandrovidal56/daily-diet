accf1ce972070df7350e9d20db01cbe3d42fa532
LeandroVidal56 <leandrovidal56@gmail.com>
2023-01-02 20:02:21 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </Container>
+    
+  );
+}
+
+
