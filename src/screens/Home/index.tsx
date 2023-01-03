0acb08377dd01d253e2d4313f12cdef62f703ae8
LeandroVidal56 <leandrovidal56@gmail.com>
2023-01-02 21:11:27 -0300

    path-imports-and-create-components

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Container } from './styles';
+import { Container, Button, Line, Input, PercentageCard, SummaryCard, SummaryCardSmall } from './styles';
 
 export function Home() {
   return (
     <Container>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button/>
+      <Line/>
+      <Input/>
+      <PercentageCard/>
+      <SummaryCard/>
+      <SummaryCardSmall/>
     </Container>
     
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -6,4 +6,58 @@ export const Container = styled.View`
     justify-content: center;
     background-color: #fff;
 `
+export const PercentageCard = styled.View`
+    width: 327px;
+    height: 102px;
+    border-radius: 8px;
+    background: #E5F0DB;
+    display: flex;
+    align-items: center; 
+    justify-content: center;
+`
+export const Button = styled.View`
+    width: 327px;
+    height: 50px;
+    border-radius: 6px;
+    background: #333638;
+    display: flex;
+    align-items: center;
+`
 
+export const Line = styled.View`
+    width: 327px;
+    height: 49px;
+    border-radius: 6px;
+    background: #DDDEDF;
+    border: 1px solid #DDDEDF;
+    display: flex;
+    flex-direction: row;
+`
+
+export const SummaryCard = styled.View`
+    width: 327px;
+    height: 89px;
+    border-radius: 8px;
+    background: #EFF0F0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const SummaryCardSmall = styled.View`
+    width: 157.5px;
+    height: 107px;
+    border-radius: 8px;
+    background: #E5F0DB;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Input = styled.View`
+    width: 327px;
+    height: 48px;
+    border-radius: 6px;
+    background: #DDDEDF;
+    border: 1px solid #DDDEDF;
+
+`
